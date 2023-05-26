080c60bc6be1b9d405fce498f93a0095645b03fe
Jonatan Goulart <jonatan784@gmail.com>
2023-05-25 22:43:17 -0300

    Refatoração utilizando Ionic

--- README.md
+++
@@ -1,3 +0,0 @@
-# Conecta Pet
-
-App para acompanhamento médico e despesas com o pet utilizando React e Vite
--- api/Db.jsx
+++
@@ -1,9 +0,0 @@
-import { Client } from "pg";
-
-export const db = new Client({
-  host: "my.database-server.com",
-  port: 5334,
-  database: "database-name",
-  user: "database-user",
-  password: "secretpassword!!",
-});
